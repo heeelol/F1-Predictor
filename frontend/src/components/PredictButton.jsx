05337ab5aba6545f39d2482650d2e172b428d4f5
NinJaWay <130268824+heeelol@users.noreply.github.com>
2025-07-17 22:44:46 +0800

    added tailwind css dependencies for styling

--- frontend/src/components/PredictButton.jsx
+++ frontend/src/components/PredictButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PredictButton( {onClick} ) {
     return (
         <section>
-            <button onClick={onClick} className="bg-blue-500">Predict Ranking</button>
+            <button onClick={onClick}>Predict Ranking</button>
         </section>
     )
 }
